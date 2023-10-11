fbc011b6a02d2150308198fd56f66c65d8e2e7eb
dotero-dev <doterodev@gmail.com>
2023-10-11 16:29:50 +0200

    Sincronización con NOTION con éxito

---
+++ src/js/notion.js
@@ -0,0 +1,32 @@
+import { Client } from '@notionhq/client'
+
+const DATABASE_ID = "888b9190ca0b410a89838294b3e596fb"
+
+const notion = new Client({
+    auth: import.meta.env.NOTION_TOKEN
+})
+
+export const getArticles = async ({ filterBy } = {}) => {
+    const query = {database_id: DATABASE_ID}
+
+    if (filterBy) {
+        query.filter = {
+            property: 'slug',
+            rich_text: {
+                equals: filterBy
+            }
+        }
+    }
+
+    const { results } = await notion.databases.query(query)
+
+    return results.map(page => {
+        const {properties} = page
+        const {slug, title} = properties
+
+        return {
+            id: slug.rich_text[0].plain_text,
+            title: title.title[0].plain_text
+        }
+    })
+}
